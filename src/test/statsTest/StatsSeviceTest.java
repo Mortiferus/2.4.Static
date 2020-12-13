63d3a777e11422acd123a8e186658aecc9fd458a
Nikita Reshetnikov <60236568+Mortiferus@users.noreply.github.com>
2020-12-13 16:58:35 +0300

    Update StatsSeviceTest.java

--- src/test/statsTest/StatsSeviceTest.java
+++ src/test/statsTest/StatsSeviceTest.java
@@ -58,3 +58,53 @@ class StatsServiceTest {
         int actual = service.salesAboveAverage();
         assertEquals(expected, actual);
     }
+
+    /* --------------------------------------- */
+
+    /* Вспомогательные и дополнительные тесты */
+
+    /* Продажа максимум */
+    @Test
+    void shouldCalculateMaximumSaleValue() {
+        StatsService service = new StatsService();
+        int expected = 20;
+        int actual = service.salesMaximum();
+        assertEquals(expected, actual);
+    }
+
+    /* Продажа минимум */
+    @Test
+    void shouldCalculateMinimumSaleValue() {
+        StatsService service = new StatsService();
+        int expected = 7;
+        int actual = service.salesMinimum();
+        assertEquals(expected, actual);
+    }
+
+    /* Месяц продажи минимум способ 2 */
+    @Test
+    void shouldSpecifyMinimumSalesMonthWay2() {
+        StatsService service = new StatsService();
+        int expected = 9;
+        int actual = service.salesMinimumMonthWay2();
+        assertEquals(expected, actual);
+    }
+
+    /* Месяц продажи максимум способ 2 */
+    @Test
+    void shouldSpecifyMaximumSalesMonthWay2() {
+        StatsService service = new StatsService();
+        int expected = 8;
+        int actual = service.salesMaximumMonthWay2();
+        assertEquals(expected, actual);
+    }
+
+    /* Месяц продажи максимум способ 3 */
+    @Test
+    void shouldSpecifyMaximumSalesMonthWay3() {
+        StatsService service = new StatsService();
+        int expected = 8;
+        int actual = service.salesMaximumMonthWay3();
+        assertEquals(expected, actual);
+    }
+}
